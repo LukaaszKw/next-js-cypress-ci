786873411df4ed69d3754f2ee3d7df44c2d620b4
LukaaszKw <l.kwiecien@bsgroup.eu>
2023-05-28 12:21:29 +0200

    add tailwind css to cypress config

--- src/components/Counter/Counter.cy.tsx
+++ src/components/Counter/Counter.cy.tsx
@@ -1,5 +1,5 @@
 import { mount } from '@cypress/react18';
-import Counter from '../Counter/Counter';
+import Counter from './Counter';
 
 describe('Counter component tests', () => {
   it('Shoud be mounted', () => {
