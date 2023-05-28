097ee144e7a08f46b309ac706c6133c3467f0515
IvanTurchyn <ivan.turchyn@interia.pl>
2023-05-28 12:47:29 +0200

    Add HorizontalUI component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import HorizonUI from '@/components/HorizonUI/HorizonUI'
-import Image from 'next/image'
 import React from 'react'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <link rel="stylesheet" href="https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/css/main.ad49aa9b.css" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
       <HorizonUI />
     </main>
   )
