930c3bbbdbf3fa4adad1624746b92bb24ba4893c
IvanTurchyn <ivan.turchyn@interia.pl>
2023-06-17 09:01:37 +0200

    Resolved conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FancyButton from '@/components/FancyButton/FancyButton'
 import HorizonUI from '@/components/HorizonUI/HorizonUI'
 import React from 'react'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <FancyButton />
       <HorizonUI />
     </main>
   )
