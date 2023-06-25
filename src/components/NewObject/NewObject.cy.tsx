517764b5d756ae589a0d0378130ab14620f0c438
Cobramarcin <cobramar@gmail.com>
2023-06-25 14:29:34 +0200

    New Object

---
+++ src/components/NewObject/NewObject.cy.tsx
@@ -0,0 +1,15 @@
+import { mount } from '@cypress/react18';
+import NewObject from '../NewObject/NewObject';
+
+describe('FancyButton component tests', () => {
+  it('Shoud be mounted', () => {
+    mount(<NewObject />);
+  });
+
+  it('Shoud be visible and have text', () => {
+    mount(<NewObject />);
+    cy.get("[data-cy-id='button']")
+    .should('be.visible')
+    .and('contain', 'New Object');
+  });
+});
