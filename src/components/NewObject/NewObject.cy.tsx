0093b15ea9d872fb5631c9866c0ebe34f6dc5dd0
Cobramarcin <cobramar@gmail.com>
2023-07-02 13:29:15 +0200

    Update NewObject.cy.tsx

--- src/components/NewObject/NewObject.cy.tsx
+++ src/components/NewObject/NewObject.cy.tsx
@@ -4,13 +4,9 @@ import NewObject from '../NewObject/NewObject';
 describe('NewObject component tests', () => {
   beforeEach(() => {
     mount(<NewObject />);
-  })
-  it('Shoud be mounted', () => {
-   // mount(<NewObject />);
   });
 
   it('Shoud be visible and have text', () => {
-   // mount(<NewObject />);
     cy.get("[data-cy-id='button']")
     .should('be.visible')
     .and('contain', 'NewObject');
