ae09cdb4f177389041590258b1e46d4868ad7d3d
Adamczak <adamus.pl@gmail.com>
2023-07-02 13:44:38 +0200

    after changes

--- src/components/HoverButton/HoverButton.cy.tsx
+++ src/components/HoverButton/HoverButton.cy.tsx
@@ -2,18 +2,18 @@ import { mount } from '@cypress/react18';
 import HoverButton from '../HoverButton/HoverButton';
 
 describe('Hover Button component tests', () => {
-    it('Should be mounted', () => {
+    beforeEach('Should be mounted', () => {
     mount(<HoverButton />);
     });
     
-    it('Check if button is enabled', () => {
+    afterEach('Check if button is enabled', () => {
     mount(<HoverButton />);
     cy.get('button').should('be.enabled')
     });
 
     it('Check if button contains text', () => {
     mount(<HoverButton />);
-    cy.get('button').should('have.text', 'Naciśnij!')
+    cy.get("[button-id='button']").should('have.text', 'Naciśnij!')
     });
 
 });
--- src/components/HoverButton/HoverButton.tsx
+++ src/components/HoverButton/HoverButton.tsx
@@ -3,7 +3,7 @@
 export default function HoverButton() {
  
   return (
-    <button className="group rounded-2xl h-12 w-48 bg-green-500 font-bold text-lg text-white relative overflow-hidden">
+    <button button-id="button" className="group rounded-2xl h-12 w-48 bg-green-500 font-bold text-lg text-white relative overflow-hidden">
         Naciśnij!
         <div className="absolute duration-300 inset-0 w-full h-full transition-all scale-0 group-hover:scale-100 group-hover:bg-white/30 rounded-2xl">
         </div>
