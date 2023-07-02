c1360eebd51a589d53808497f3fc826b43ad45ff
IvanTurchyn <ivan.turchyn@interia.pl>
2023-07-02 12:17:19 +0200

    Using data-* attributes to select elements

--- src/components/HorizonUI/HorizonUI.cy.tsx
+++ src/components/HorizonUI/HorizonUI.cy.tsx
@@ -10,31 +10,31 @@ describe('HorizonUI Component', () => {
   
     // This test checks if the HorizonUI component renders correctly by checking if an element with the class 'horizon-ui' is visible.
     it('should render the component', () => {
-      cy.get('.horizon-ui').should('be.visible');
+      cy.get('[data-cy="horizon-ui"]').should('be.visible');
     });
   
     // This test checks if the 'Horizon UI Inputs' heading is correctly rendered by checking if a h4 element containing the text 'Horizon UI Inputs' is visible.
     it('should render the Horizon UI Inputs heading', () => {
-      cy.get('h4').contains('Horizon UI Inputs').should('be.visible');
+      cy.contains('Horizon UI Inputs').should('be.visible');
     });
   
     // This test checks if the Default input field is correctly rendered by checking if an input element with the id 'email' is visible.
     it('should render the Default input', () => {
-      cy.get('input#email').should('be.visible');
+      cy.get('[data-cy="default-input"]').should('be.visible');
     });
   
     // This test checks if the Success input field is correctly rendered by checking if an input element with the id 'email2' is visible.
     it('should render the Success input', () => {
-      cy.get('input#email2').should('be.visible');
+      cy.get('[data-cy="success-input"]').should('be.visible');
     });
   
     // This test checks if the Error input field is correctly rendered by checking if an input element with the id 'email3' is visible.
     it('should render the Error input', () => {
-      cy.get('input#email3').should('be.visible');
+      cy.get('[data-cy="error-input"]').should('be.visible');
     });
   
     // This test checks if the Disabled input field is correctly rendered by checking if an input element with the id 'email4' is disabled.
     it('should render the Disabled input', () => {
-      cy.get('input#email4').should('be.disabled');
+      cy.get('[data-cy="disabled-input"]').should('be.disabled');
     });
-  });  
+  });
--- src/components/HorizonUI/HorizonUI.tsx
+++ src/components/HorizonUI/HorizonUI.tsx
@@ -1,29 +1,29 @@
 export default function HorizonUI(){
     return(
-        <div className="horizon-ui flex flex-col justify-center items-center h-[100vh]">
-        <div className="!z-5 relative flex flex-col rounded-[20px] max-w-[300px] md:max-w-[400px] bg-white bg-clip-border shadow-3xl shadow-shadow-500 flex flex-col w-full !p-6 3xl:p-![18px] bg-white undefined">
-            <div className="relative flex flex-row justify-between">
-                <h4 className="text-xl font-bold text-navy-700 dark:text-white mb-3">
-                    Horizon UI Inputs
-                </h4>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="email" className="text-sm text-navy-700 dark:text-white font-bold">Default</label>
-                <input type="text" id="email" placeholder="@horizon.ui" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-gray-200"/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="email2" className="text-sm text-navy-700 dark:text-white font-bold">Success</label>
-                <input type="text" id="email2" placeholder="Success input" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-green-500 text-green-500 placeholder:text-green-500 dark:!border-green-400 dark:!text-green-400 dark:placeholder:!text-green-400"/>
-            </div>
-            <div className="mb-3">
-                <label htmlFor="email3" className="text-sm text-navy-700 dark:text-white font-bold">Error</label>
-                <input type="text" id="email3" placeholder="Error input" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-red-500 text-red-500 placeholder:text-red-500 dark:!border-red-400 dark:!text-red-400 dark:placeholder:!text-red-400"/>
-            </div>
-            <div>
-                <label htmlFor="email4" className="text-sm text-navy-700 dark:text-white font-bold">Disabled</label>
-                <input disabled type="text" id="email4" placeholder="@horizon.ui" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-none bg-gray-100 cursor-not-allowed dark:bg-white/5" />
+        <div className="horizon-ui flex flex-col justify-center items-center h-[100vh]" data-cy="horizon-ui">
+            <div className="!z-5 relative flex flex-col rounded-[20px] max-w-[300px] md:max-w-[400px] bg-white bg-clip-border shadow-3xl shadow-shadow-500 flex flex-col w-full !p-6 3xl:p-![18px] bg-white undefined">
+                <div className="relative flex flex-row justify-between">
+                    <h4 className="text-xl font-bold text-navy-700 dark:text-white mb-3">
+                        Horizon UI Inputs
+                    </h4>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="text-sm text-navy-700 dark:text-white font-bold">Default</label>
+                    <input type="text" id="email" placeholder="@horizon.ui" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-gray-200" data-cy="default-input"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email2" className="text-sm text-navy-700 dark:text-white font-bold">Success</label>
+                    <input type="text" id="email2" placeholder="Success input" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-green-500 text-green-500 placeholder:text-green-500 dark:!border-green-400 dark:!text-green-400 dark:placeholder:!text-green-400" data-cy="success-input"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email3" className="text-sm text-navy-700 dark:text-white font-bold">Error</label>
+                    <input type="text" id="email3" placeholder="Error input" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-red-500 text-red-500 placeholder:text-red-500 dark:!border-red-400 dark:!text-red-400 dark:placeholder:!text-red-400" data-cy="error-input"/>
+                </div>
+                <div>
+                    <label htmlFor="email4" className="text-sm text-navy-700 dark:text-white font-bold">Disabled</label>
+                    <input disabled type="text" id="email4" placeholder="@horizon.ui" className="mt-2 flex h-12 w-full items-center justify-center rounded-xl border bg-white/0 p-3 text-sm outline-none border-none bg-gray-100 cursor-not-allowed dark:bg-white/5" data-cy="disabled-input" />
+                </div>
             </div>
         </div>
-    </div>
     );
 }
