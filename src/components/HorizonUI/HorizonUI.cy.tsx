ec590ff2671a7dc4a8fe2bf185255b54ed921319
IvanTurchyn <ivan.turchyn@interia.pl>
2023-07-02 12:25:35 +0200

    Used aliases and closures to access

--- src/components/HorizonUI/HorizonUI.cy.tsx
+++ src/components/HorizonUI/HorizonUI.cy.tsx
@@ -3,10 +3,14 @@ import HorizonUI from '@/components/HorizonUI/HorizonUI';
 
 // Group of tests for the HorizonUI component.
 describe('HorizonUI Component', () => {
-    // This function runs before each test. It mounts the HorizonUI component to the DOM, preparing it for the tests.
-    beforeEach(() => {
-      mount(<HorizonUI />);
-    });
+  // This function runs before each test. It mounts the HorizonUI component to the DOM, preparing it for the tests.
+  beforeEach(() => {
+    mount(<HorizonUI />);
+    cy.get('input#email').as('defaultInput');
+    cy.get('input#email2').as('successInput');
+    cy.get('input#email3').as('errorInput');
+    cy.get('input#email4').as('disabledInput');
+  });
   
     // This test checks if the HorizonUI component renders correctly by checking if an element with the class 'horizon-ui' is visible.
     it('should render the component', () => {
@@ -20,21 +24,21 @@ describe('HorizonUI Component', () => {
   
     // This test checks if the Default input field is correctly rendered by checking if an input element with the id 'email' is visible.
     it('should render the Default input', () => {
-      cy.get('[data-cy="default-input"]').should('be.visible');
+      cy.get('@defaultInput').should('be.visible');
     });
-  
+
     // This test checks if the Success input field is correctly rendered by checking if an input element with the id 'email2' is visible.
     it('should render the Success input', () => {
-      cy.get('[data-cy="success-input"]').should('be.visible');
+      cy.get('@successInput').should('be.visible');
     });
-  
+
     // This test checks if the Error input field is correctly rendered by checking if an input element with the id 'email3' is visible.
     it('should render the Error input', () => {
-      cy.get('[data-cy="error-input"]').should('be.visible');
+      cy.get('@errorInput').should('be.visible');
     });
-  
+
     // This test checks if the Disabled input field is correctly rendered by checking if an input element with the id 'email4' is disabled.
     it('should render the Disabled input', () => {
-      cy.get('[data-cy="disabled-input"]').should('be.disabled');
+      cy.get('@disabledInput').should('be.disabled');
     });
   });
