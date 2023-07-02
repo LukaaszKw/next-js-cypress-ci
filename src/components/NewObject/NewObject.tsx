77ea94795174ad80678097c82e4d981e41bab656
Cobramarcin <cobramar@gmail.com>
2023-07-02 13:46:46 +0200

    Update NewObject.tsx

--- src/components/NewObject/NewObject.tsx
+++ src/components/NewObject/NewObject.tsx
@@ -1,12 +1,11 @@
 'use client';
  
-import React, { useState } from 'react';
+import React from 'react';
  
 export default function NewObject() {
- 
-  return (
+   return (
     <div className="relative flex min-h-screen flex-col space-y-10 justify-center overflow-hidden py-6 sm:py-12">
-    <button data-cy-id="button" className="bg-gradient-to-b w-max mx-auto text-blue-500 font-semibold from-slate-50 to-blue-100 px-10 py-3 rounded-2xl shadow-blue-400 shadow-md border-b-4 hover border-b border-blue-200 hover:shadow-sm transition-all duration-500">NewObject</button>
+    <button data-cy-id="button" className="bg-gradient-to-b w-max mx-auto text-green-500 font-semibold from-slate-50 to-red-100 px-10 py-30 rounded-1x2 shadow-red-400 shadow-md border-r-4 hover border-b border-red-200 hover:shadow-sm transition-all duration-500">NewObject</button>
     </div>
   );
 }
