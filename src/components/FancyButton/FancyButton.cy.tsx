2eee67d74f2d7ca53fe59c07131449c2432633fa
Adamczak <adamus.pl@gmail.com>
2023-07-02 13:13:18 +0200

    beforeEach added

--- src/components/FancyButton/FancyButton.cy.tsx
+++ src/components/FancyButton/FancyButton.cy.tsx
@@ -2,14 +2,14 @@ import { mount } from '@cypress/react18';
 import FancyButton from '../FancyButton/FancyButton';
 
 describe('FancyButton component tests', () => {
-  it('Shoud be mounted', () => {
+  beforeEach('Shoud be mounted', () => {
     mount(<FancyButton />);
   });
 
   it('Shoud be visible and have text', () => {
     mount(<FancyButton />);
     cy.get("[data-cy-id='button']")
-    .should('be.visible')
-    .and('contain', 'Fancy button');
+    cy.should('be.visible')
+    cy.and('contain', 'Fancy button');
   });
 });
